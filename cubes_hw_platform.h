82779308e4bf037d9a66fd68773cb1326b50bc0e
Theodor Stana <stana@kth.se>
2018-11-06 11:42:33 +0100

    Add I2C files and links to programming files in README

---
+++ cubes_hw_platform.h
@@ -0,0 +1,16 @@
+#ifndef cubes_HW_PLATFORM_H_
+#define cubes_HW_PLATFORM_H_
+/*****************************************************************************
+*
+*Created by Microsemi SmartDesign  Tue Nov 06 11:19:02 2018
+*
+*Memory map specification for peripherals in cubes
+*/
+
+/*-----------------------------------------------------------------------------
+* CM3 subsystem memory map
+* Master(s) for this subsystem: CM3 
+*---------------------------------------------------------------------------*/
+
+
+#endif /* cubes_HW_PLATFORM_H_*/
